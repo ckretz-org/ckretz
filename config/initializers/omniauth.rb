4c8131045a75d29308ca0e35e8c84e98509c8639
Mohammed O. Tillawy <tillawy@gmail.com>
2024-01-07 08:17:42 +0300

    fix env variables handling

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
-  provider :google_oauth2, ENV.fetch("GOOGLE_AUTH_CLIENT_ID"), ENV.fetch("GOOGLE_AUTH_CLIENT_SECRET")
+  provider :google_oauth2, ENV.fetch("GOOGLE_AUTH_CLIENT_ID", "SETUP_ME_UP_PLZ"), ENV.fetch("GOOGLE_AUTH_CLIENT_SECRET", "SETUP_ME_UP_PLZ")
 end
 
 OmniAuth.config.logger = Rails.logger
