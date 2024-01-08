857e3818c64ce3493fec41fe0ea2c7aad21cb973
Mohammed O. Tillawy <tillawy@gmail.com>
2024-01-08 07:25:11 +0300

    add index to column: secrets->name

--- db/migrate/20240106175352_create_secrets.rb
+++ db/migrate/20240106175352_create_secrets.rb
@@ -1,10 +1,11 @@
 class CreateSecrets < ActiveRecord::Migration[7.1]
   def change
     create_table :secrets, id: :uuid do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: true
       t.string :value, null: false
-      t.references :user, null: false, type: :uuid
+      t.references :user, null: false, type: :uuid, index: true
       t.timestamps
     end
+    add_index :secrets, [ :user_id, :name ], unique: true
   end
 end
