c2aad1a2854c90310d5966f78a44cadbe26ea31c
Mohammed O. Tillawy <tillawy@gmail.com>
2024-01-12 18:18:45 +0300

    handle authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  rescue_from Exceptions::NotAuthorized, with: :not_authorized
+
+  private
+  def not_authorized
+    redirect_to welcome_path
+  end
 end
