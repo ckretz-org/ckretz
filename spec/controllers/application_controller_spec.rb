a232c9ae6501f734063752473a98348825325a02
Mohammed O. Tillawy <tillawy@gmail.com>
2024-01-12 18:30:54 +0300

    coverage

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ApplicationController, type: :request do
+
+  describe "rescue_from" do
+    before do
+      allow_any_instance_of(AccessTokensController).to receive(:current_user).and_raise(Exceptions::NotAuthorized)
+    end
+    it do
+      get access_tokens_path
+      expect(response.location).to eql("http://www.example.com/welcome")
+    end
+  end
+end
