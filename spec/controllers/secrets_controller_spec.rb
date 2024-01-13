0976dc3076bee3d8f3634dffb2740dc89976d180
Mohammed O. Tillawy <tillawy@gmail.com>
2024-01-13 07:54:48 +0300

    test secrets_controller show

--- spec/controllers/secrets_controller_spec.rb
+++ spec/controllers/secrets_controller_spec.rb
@@ -5,12 +5,14 @@ require "rails_helper"
 RSpec.describe SecretsController, type: :request do
   let(:current_user) { build(:user) }
   let(:secret) { build(:secret, user: current_user) }
+  let(:current_user_secrets) { class_double(Secret) }
 
   before do
     allow_any_instance_of(described_class).to receive(:session).and_return({ current_user_id: current_user.id })
     allow(User).to receive(:find_by_id).with(current_user.id).and_return(current_user)
-
-
+    allow(current_user).to receive(:secrets).and_return(current_user_secrets)
+    allow(current_user_secrets).to receive(:new).and_return(secret)
+    allow(current_user_secrets).to receive(:find).with(secret.id).and_return(secret)
   end
 
   describe "index" do
@@ -19,6 +21,14 @@ RSpec.describe SecretsController, type: :request do
     end
     it do
       get secrets_path
+      expect(response.status).to eql(200)
+      expect(response.body).to include(secret.name)
+    end
+  end
+
+  describe "show" do
+    it do
+      get secret_path(id: secret.id)
       expect(response.status).to eql(200)
       expect(response.body).to include(secret.name)
     end
