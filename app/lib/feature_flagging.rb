1ffa8b8c6aba01c9b2086b5128ba691dc05875ed
Mohammed O. Tillawy <tillawy@gmail.com>
2024-05-11 23:24:39 +0300

    debug message

--- app/lib/feature_flagging.rb
+++ app/lib/feature_flagging.rb
@@ -3,6 +3,8 @@ class FeatureFlagging
     OpenFeature::SDK::API.instance.build_client
   end
   def self.fetch_boolean_flag(key:, default: true)
-    client.fetch_boolean_value(flag_key: key, default_value: default)
+    output = client.fetch_boolean_value(flag_key: key, default_value: default)
+    Rails.logger.info("Feature flag #{key} is set to #{output}")
+    output
   end
 end
