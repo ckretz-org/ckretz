5297b7dcafabb7ca1022a3270a1b474b172b0e02
Mohammed O. Tillawy <tillawy@gmail.com>
2024-05-12 08:48:51 +0300

    potential work-around for puma & grpc issue

--- config/initializers/openfeature.rb
+++ config/initializers/openfeature.rb
@@ -36,11 +36,11 @@ class OpenFeature::FlagD::Provider::Client
   end
 end
 
-OpenFeature::SDK.configure do |config|
-  client = OpenFeature::FlagD::Provider.build_client do |client|
-    client.host = ENV.fetch("FLAGD_HOST", "localhost")
-    client.port = ENV.fetch("FLAGD_PORT", "8013")
-    client.tls = ENV.fetch("FLAGD_TLS", "false") == "true"
-  end
-  config.set_provider(client)
-end
+# OpenFeature::SDK.configure do |config|
+#   client = OpenFeature::FlagD::Provider.build_client do |client|
+#     client.host = ENV.fetch("FLAGD_HOST", "localhost")
+#     client.port = ENV.fetch("FLAGD_PORT", "8013")
+#     client.tls = ENV.fetch("FLAGD_TLS", "false") == "true"
+#   end
+#   config.set_provider(client)
+# end
--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,14 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+on_worker_boot do
+  OpenFeature::SDK.configure do |config|
+    client = OpenFeature::FlagD::Provider.build_client do |client|
+      client.host = ENV.fetch("FLAGD_HOST", "localhost")
+      client.port = ENV.fetch("FLAGD_PORT", "8013")
+      client.tls = ENV.fetch("FLAGD_TLS", "false") == "true"
+    end
+    config.set_provider(client)
+  end
+end
