71548d513475b1241a3486d90cfa7b6065dd282a
Mohammed O. Tillawy <tillawy@gmail.com>
2024-06-06 10:16:20 +0300

    refactor, use stimulus instead of eventListener

--- app/javascript/controllers/top_bar_controller.js
+++ app/javascript/controllers/top_bar_controller.js
@@ -8,4 +8,7 @@ export default class extends Controller {
     toggleDarkMode() {
         document.querySelector('html').classList.toggle('dark');
     }
+    toggleDropdown() {
+        document.querySelector('#user-options-dropdown-options').classList.toggle('hidden');
+    }
 }
