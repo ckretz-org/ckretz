8f9b77f97a4b9f8079f47d16bed37a0ee4da1084
Mohammed O. Tillawy <tillawy@gmail.com>
2024-06-12 20:09:26 +0300

    spec issue

--- spec/factories/secrets.rb
+++ spec/factories/secrets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :secret do
     id { SecureRandom.uuid }
-    name { Faker::Alphanumeric.alpha(number: 10).sub(" ", "_") }
+    name { Faker::Alphanumeric.alpha(number: 10) }
     created_at { Time.now }
     user
   end
