a8a2d704fbe3632de21884083319923d5371f32a
Mohammed O. Tillawy <tillawy@gmail.com>
2024-07-27 10:29:54 +0300

    install pg_stat_statements

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2024_06_14_105028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
+  enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "vector"
