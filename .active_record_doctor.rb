86f01c13ecc426eab9da443d76c48ac41755fa36
Mohammed O. Tillawy <tillawy@gmail.com>
2024-09-12 09:39:20 +0300

    active_record_doctor does not support multi-database

--- .active_record_doctor.rb
+++ .active_record_doctor.rb
@@ -9,18 +9,20 @@ ActiveRecordDoctor.configure do
     "SolidQueue::ReadyExecution",
     "SolidQueue::RecurringExecution",
     "SolidQueue::ScheduledExecution",
+    "SolidQueue::BlockedExecution",
+    "SolidQueue::ClaimedExecution",
+    "SolidQueue::FailedExecution",
+    "SolidQueue::RecurringTask",
     "ActionText::RichText",
     "ActionText::EncryptedRichText",
     "ActiveStorage::VariantRecord",
     "ActiveStorage::Blob",
     "ActiveStorage::Attachment",
-    "ActionMailbox::InboundEmail"
+    "ActionMailbox::InboundEmail",
   ]
   global :ignore_tables, [
     "ar_internal_metadata",
     "schema_migrations",
-    "solid_queue_claimed_executions",
-    "solid_queue_processes",
-    "solid_queue_jobs"
+    /^solid_queue/
   ]
 end
