22962fe5f1b6bf42e70a743d602bc83aa8a17a79
Mohammed O. Tillawy <tillawy@gmail.com>
2024-09-12 13:56:54 +0300

    potential fix for github actions

--- config/initializers/prosopite.rb
+++ config/initializers/prosopite.rb
@@ -1,7 +1,7 @@
 unless Rails.env.production?
   require "prosopite/middleware/rack"
   Rails.configuration.middleware.use(Prosopite::Middleware::Rack)
+  # Prosopite.allow_stack_paths = ['substring_in_call_stack', /regex/]
+  Prosopite.ignore_queries = [/current_setting/]
 end
 
-# Prosopite.allow_stack_paths = ['substring_in_call_stack', /regex/]
-Prosopite.ignore_queries = [ /current_setting/ ]
