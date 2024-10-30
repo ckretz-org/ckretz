3007e8a3a9996fd629363f7557c8d1b0cc88ff88
Mohammed O. Tillawy <tillawy@gmail.com>
2024-10-30 23:12:58 +0300

    work on dagger

--- dagger/main.go
+++ dagger/main.go
@@ -16,10 +16,10 @@ package main
 
 import (
 	"context"
+	"dagger/ckretz/internal/dagger"
 	"fmt"
 	"math"
 	"math/rand"
-	"dagger/ckretz/internal/dagger"
 )
 
 type Ckretz struct{}
@@ -30,24 +30,52 @@ func (m *Ckretz) Publish(ctx context.Context, source *dagger.Directory) (string,
 	if err != nil {
 		return "", err
 	}
-    return m.Build(ctx, source).
+	return m.Build(ctx, source).
 		Publish(ctx, fmt.Sprintf("ttl.sh/ckretz-app-arm-dagger-%.0f", math.Floor(rand.Float64()*10000000)))
 }
 
 // Build the application container
 func (m *Ckretz) Build(ctx context.Context, source *dagger.Directory) *dagger.Container {
- 	return dag.Container().
- 	WithDirectory("/rails", source).
- 	WithWorkdir("/rails").
- 	Directory("/rails").
- 	DockerBuild()
+	return dag.Container().
+		WithDirectory("/rails", source).
+		WithWorkdir("/rails").
+		Directory("/rails").
+		DockerBuild()
 	//return m.BuildEnv(source)
+}
+
+func (m *Ckretz) Postgres(ctx context.Context) *dagger.Container {
+	return dag.Container().
+		From("ankane/pgvector").
+		WithEnvVariable("POSTGRES_USER", "postgres").
+		WithEnvVariable("POSTGRES_PASSWORD", "password").
+		WithExposedPort(5432)
+}
+
+func (m *Ckretz) PostgresService(ctx context.Context) *dagger.Service {
+	return m.Postgres(ctx).AsService()
 }
 
 // Return the result of running unit tests
 func (m *Ckretz) Test(ctx context.Context, source *dagger.Directory) (string, error) {
 	return m.BuildEnv(source).
-		WithExec([]string{"bundle", "exec", "bin/rubocop"}).
+		WithServiceBinding("db", m.PostgresService(ctx)). // bind database with the name db
+		WithEnvVariable("DB_HOST", "db").                 // db refers to the service binding
+		WithEnvVariable("DATABASE_PRIMARY_RW_URL", "postgresql://db:5432/").
+		WithEnvVariable("DATABASE_CACHE_RW_URL", "postgresql://db:5432/").
+		WithEnvVariable("DATABASE_QUEUE_RW_URL", "postgresql://db:5432/").
+		WithEnvVariable("DATABASE_CABLE_RW_URL", "postgresql://db:5432/").
+		WithEnvVariable("ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY", "deterministickeyaaaaaaaaaaaaaaaa").
+		WithEnvVariable("ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT", "keyderivationsaltbbbbbbbbbbbbbbb").
+		WithEnvVariable("ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY", "primarykeycccccccddddddddddddddd").
+		WithEnvVariable("COVERAGE", "0").
+		WithEnvVariable("RAILS_ENV", "test").
+		WithEnvVariable("DB_PASSWORD", "password"). // password set in db container
+		WithEnvVariable("DB_USERNAME", "postgres"). // default user in postgres image
+		WithEnvVariable("DB_NAME", "postgres").     // default db name in postgres image
+		WithExec([]string{"bundle", "exec", "rake", "db:setup"}).
+		WithExec([]string{"bundle", "exec", "rspec"}).
+		WithExec([]string{"bundle", "exec", "rubocop"}).
 		WithExec([]string{"bundle", "exec", "bundle-audit"}).
 		WithExec([]string{"bundle", "exec", "brakeman"}).
 		Stdout(ctx)
