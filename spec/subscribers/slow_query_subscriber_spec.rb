00d41efe513d04147b3518630da5042d5136f32a
Mohammed O. Tillawy <tillawy@gmail.com>
2025-11-13 17:35:43 +0300

    rubocop

--- spec/subscribers/slow_query_subscriber_spec.rb
+++ spec/subscribers/slow_query_subscriber_spec.rb
@@ -4,12 +4,13 @@ require 'rails_helper'
 
 RSpec.describe SlowQuerySubscriber do
   before do
-    SlowQuerySubscriber
+    described_class
     allow(Rails.logger).to receive(:warn).once.with(/slow_query/)
   end
+
   context 'when condition' do
     it 'succeeds' do
-      ActiveRecord::Base.connection.execute("select PG_SLEEP(0.2)")
+      expect { ActiveRecord::Base.connection.execute("select PG_SLEEP(0.2)") }.not_to raise_error
     end
   end
 end
