f92cdbc50c4064258e487c960c1812fbd8b92668
Mohammed O. Tillawy <tillawy@gmail.com>
2025-11-15 14:17:10 +0300

    track logout issue, verbose logging

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -13,12 +13,18 @@ module CurrentUser
 
       token = request.headers["Authorization"].split(" ").last
       access_token = AccessToken.find_by_token(token)
-      raise Exceptions::NotAuthorized unless access_token
+      unless access_token
+        Rails.logger.info "Access token not found: #{token}"
+        raise Exceptions::NotAuthorized
+      end
       access_token.user
     end
 
     def current_session_user
-      raise Exceptions::NotAuthorized unless session[:current_user_id]
+      unless session[:current_user_id]
+        Rails.logger.info "No session found for current_user"
+        raise Exceptions::NotAuthorized
+      end
 
       User.find_by_id(session[:current_user_id])
     end
